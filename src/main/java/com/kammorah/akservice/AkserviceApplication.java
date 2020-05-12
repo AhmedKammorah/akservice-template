73849ff0bf93adb8edbc0f3fbbfa1dfaf98fc724
Ahmed kammorah <Ahmedkammorah@gmail.com>
2020-05-12 20:22:02 +0200

    initial template project

---
+++ src/main/java/com/kammorah/akservice/AkserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kammorah.akservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kammorah/akservice/AkserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kammorah.akservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
